d463c017b4ec5ed75ed2baf6efc8ba5eb9d7ce53
mlazarz <70230584+mlazarz@users.noreply.github.com>
2020-10-26 23:40:29 -0400

    Update Lab2.py

--- Lab2.py
+++ Lab2.py
@@ -1,9 +1,10 @@
 # Author:  Mitchell Lazarz
 # Python 2.7, Lab2.py
 # Creation Date:  26 October 2020
-# Description:  This code performs a clip on parks and bike routes within a zip code as well as creates a 500 meter buffer around facilities.  The output can be used to inform the UN of safety concerns with bike routes within the buffer of facility locations.
+# Description:  This code performs a clip on parks and bike routes within a zip code as well as creates a 500-meter buffer around facilities.  
+# The output can be used to inform the UN of safety concerns with bike routes within the buffer of facility locations.
 
-# Add parks.shp and zip.shp to contents on .mxd manually
+# Add parks.shp and zip.shp to contents of .mxd manually
 # A clip is performed with parks as the input feature, zip as the clip feature, and the output saved as parks_Clip to a local results folder.
 arcpy.Clip_analysis("parks", "zip", "C:\\CLARK\\PythonProgramming\\CompProg_Lab2\\Results\\parks_Clip.shp")
 
@@ -13,7 +14,7 @@ from arcpy import env
 # The geoprocessing workspace path is set on local drive.
 env.workspace="C:\\CLARK\\PythonProgramming\\CompProg_Lab2"
 
-# Add facilities.shp to contents on .mxd manually
+# Add facilities.shp to contents of .mxd manually
 # A 500 meter buffer is performed with facilities as the input feature.  The output is saved to the results folder.
 arcpy.Buffer_analysis("facilities","\\Results\\facilities_buffer.shp","500 METERS")
 
@@ -22,9 +23,9 @@ arcpy.Buffer_analysis("facilities","\\Results\\facilities_buffer.shp","500 METER
 
 # Input parameters for a clip analysis are assigned to variables.
 in_features="\\Data_Lab_2_Geoprocessing_Python\\bike_routes.shp"  # Bike routes shapefile is the input feature.
-clip_features="\\Data_Lab_2_Geoprocessing_Python\\zip.shp" # Zip code shapefile is the clip feature.
-out_features="\\Results\\bike_Clip.shp" # Output feature is saved as bike_Clip to Results folder.
-xy_tolerance="" # XY tolerance is set to NULL
+clip_features="\\Data_Lab_2_Geoprocessing_Python\\zip.shp"        # Zip code shapefile is the clip feature.
+out_features="\\Results\\bike_Clip.shp"                           # Output feature is saved as bike_Clip to Results folder.
+xy_tolerance=""                                                   # XY tolerance is set to ""
 
 # The clip is performed using the previously defined variables.
 arcpy.Clip_analysis(in_features, clip_features, out_features, xy_tolerance)
